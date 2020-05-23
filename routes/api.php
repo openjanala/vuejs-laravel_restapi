1b86b2a593d3952793cb22ce410afdbedadddc75
openjanala <openjanala@gmail.com>
2020-05-23 09:05:59 +0600

    Class 38 Completed with JWT Token Login Method

--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,20 @@ Route::apiResource('/class','Api\ClassController');
 Route::apiResource('/subject','Api\SubjectController');
 Route::apiResource('/section','Api\SectionController');
 Route::apiResource('/student','Api\StudentController');
+
+
+//for jWT route =   api/auth/login
+Route::group([
+
+    // 'middleware' => 'api',
+    //'namespace' => 'App\Http\Controllers',
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
